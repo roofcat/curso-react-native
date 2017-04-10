b9e5b63b12ee98835914a4271c6a318ba52efc5b
Christian Rojas <ristiandiegor@gmail.com>
2017-04-10 10:16:09 -0300

    primer commit

---
+++ index.android.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ListView,
+  TouchableHighlight,
+} from 'react-native';
+
+export default class AwesomeProject extends Component {
+
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 != r2 });
+    this.state = {
+      dataSource: ds.cloneWithRows([])
+    }
+  }
+
+  componentDidMount() {
+    var titles = [];
+    fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        var movies = responseJson.movies;
+        for (var i = 0; i < movies.length; i++) {
+          titles.push(movies[i].title);
+        }
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(titles)
+        })
+      })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <ListView
+          enableEmptySections={true}
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+        />
+      </View>
+    );
+  }
+
+  renderRow(dataRow) {
+    return (
+      <TouchableHighlight>
+        <View style={styles.cell}>
+          <Text>{dataRow}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    paddingTop: 30,
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  cell: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+    paddingTop: 20,
+    paddingBottom: 20,
+    alignItems: 'center',
+  }
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
