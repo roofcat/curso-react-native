4446d31282030a3d77f5512f8fcb2ba3eb62da1c
Christian Rojas <ristiandiegor@gmail.com>
2017-04-11 10:14:43 -0300

    se agregan funcionalidades ios

--- index.ios.js
+++ index.ios.js
@@ -1,34 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Button,
+  ListView,
+  TouchableHighlight,
+  AlertIOS,
 } from 'react-native';
 
 export default class AwesomeProject extends Component {
+
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      dataSource: ds.cloneWithRows([])
+    }
+  }
+
+  componentDidMount() {
+    var titles = [];
+    fetch('https://facebook.github.io/react-native/movies.json')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        var movies = responseJson.movies;
+        for (var i = 0; i < movies.length; i++) {
+          titles.push(movies[i].title);
+        }
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(titles)
+        })
+      })
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <ListView
+          enableEmptySections={true}
+          dataSource={this.state.dataSource}
+          renderRow={this.renderRow}
+        />
       </View>
     );
   }
+
+  pressCell(dataRow) {
+    AlertIOS.alert(dataRow)
+  }
+
+  renderRow(dataRow) {
+    return (
+      <TouchableHighlight onPress={() => {this.pressCell(dataRow)}}>
+        <View style={styles.cell}>
+          <Text>{dataRow}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -37,6 +69,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+    paddingTop: 30,
   },
   welcome: {
     fontSize: 20,
@@ -48,6 +81,13 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  cell: {
+    borderBottomWidth: 1,
+    borderBottomColor: 'grey',
+    paddingTop: 20,
+    paddingBottom: 20,
+    alignItems: 'center',
+  }
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
